30ccfeca8582809b60846ac94e7f21233ef6656a
pinglamb <pinglambs@gmail.com>
2016-11-04 05:28:22 +0800

    Able to access CNContact from EPContact

--- Pods/EPContact.swift
+++ Pods/EPContact.swift
@@ -10,7 +10,7 @@ import UIKit
 import Contacts
 
 open class EPContact {
-    
+
     open var firstName: String
     open var lastName: String
     open var company: String
@@ -21,61 +21,65 @@ open class EPContact {
     open var contactId: String?
     open var phoneNumbers = [(phoneNumber: String, phoneLabel: String)]()
     open var emails = [(email: String, emailLabel: String )]()
-	
+
+    open var cnContact: CNContact
+
     public init (contact: CNContact) {
+        cnContact = contact
+
         firstName = contact.givenName
         lastName = contact.familyName
         company = contact.organizationName
         contactId = contact.identifier
-        
+
         if let thumbnailImageData = contact.thumbnailImageData {
             thumbnailProfileImage = UIImage(data:thumbnailImageData)
         }
-        
+
         if let imageData = contact.imageData {
             profileImage = UIImage(data:imageData)
         }
-        
+
         if let birthdayDate = contact.birthday {
-            
+
             birthday = Calendar(identifier: Calendar.Identifier.gregorian).date(from: birthdayDate)
             let dateFormatter = DateFormatter()
             dateFormatter.dateFormat = EPGlobalConstants.Strings.birdtdayDateFormat
             //Example Date Formats:  Oct 4, Sep 18, Mar 9
             birthdayString = dateFormatter.string(from: birthday!)
         }
-        
+
 		for phoneNumber in contact.phoneNumbers {
 			guard let phoneLabel = phoneNumber.label else { continue }
 			let phone = phoneNumber.value.stringValue
-			
+
 			phoneNumbers.append((phone,phoneLabel))
 		}
-		
+
 		for emailAddress in contact.emailAddresses {
 			guard let emailLabel = emailAddress.label else { continue }
 			let email = emailAddress.value as String
-			
+
 			emails.append((email,emailLabel))
 		}
     }
-	
+
     open func displayName() -> String {
         return firstName + " " + lastName
     }
-    
+
     open func contactInitials() -> String {
         var initials = String()
-		
+
 		if let firstNameFirstChar = firstName.characters.first {
 			initials.append(firstNameFirstChar)
 		}
-		
+
 		if let lastNameFirstChar = lastName.characters.first {
 			initials.append(lastNameFirstChar)
 		}
-		
+
         return initials
     }
-    
+
 }
